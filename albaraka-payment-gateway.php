d48c6d9b70ccabe8b26e488d4184071a1b13bfd9
ghader <info@tydeey.com>
2025-06-03 00:25:03 -0700

    add initial plugin files and .gitignore

---
+++ albaraka-payment-gateway.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Al Baraka Payment Gateway
+  Plugin URI: https://github.com/YourUsername/albaraka-payment-gateway-wp
+  Description: بوابة دفع مخصصة لبنك البركة (WooCommerce)
+  Version: 1.0.0
+  Author: اسمك أو اسم المشروع
+  Author URI: https://yourwebsite.com
+  Text Domain: albaraka-payment-gateway
+*/
+// هنا بيبتدي كود البلاغن...
